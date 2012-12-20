25cdbd7a30e176579aae8d221b1ac81254faba4f
tez <ns7tetsuya@gmail.com>
2012-12-20 18:31:43 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MitoFoods::Application.config.secret_token = 'fe15f8199b57febd10e9028578ae02b53dd74f70227c3e279880286a1020f1a288778d39ef33d09eca486b336a02033b13cf0944dc24ee925d25817ef9c09ec1'
